65cf83e62d9149beabb658cc6317d34a7144f854
Jonathan Cremin <jonathan@crem.in>
2016-02-28 15:54:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Home Server
+
+A small bit of automation for initial configuration of my home server.
+
+Uses docker-compose for orchestrating most services.
---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install samba netatalk avahi-daemon avahi-utils slurm cryptsetup htop bwm-ng samba git smartmontools fail2ban zsh
+curl -fsSL https://get.docker.com/ | sh
+sudo usermod -aG docker jonathan
+sudo cryptsetup luksOpen /dev/md0
+sudo cryptsetup luksOpen /dev/md0 storage
+sudo mkdir /mnt/storage
+sudo mount /dev/mapper/storage /mnt/storage
+sudo cp ./files/etc_network_interfaces /etc/network/interfaces
+sudo ifdown eth0 && sudo ifup eth0
+sudo pkill dhclient
+sudo smbpasswd -a jonathan
+chsh -s /bin/zsh
